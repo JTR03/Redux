7426fc86229a63f12a823bac5502e8f583421ec2
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-03-28 11:52:29 +0200

    variable used to hold type

--- 4.reference_var.js
+++ 4.reference_var.js
@@ -1,1 +1,42 @@
-// 
+// same code as 3.multi actions using variables references
+ import Redux from 'react-redux'
+
+ const LOGIN = 'LOGIN'
+ const LOGOUT = 'LOGOUT'
+
+ const initialState = {
+     auth: false
+ }
+
+ const reducer = (state = initialState, action) => {
+     switch(action.type){
+         case LOGIN:
+             return {
+                ...state,
+                auth: true
+             }
+        
+        case LOGOUT:
+            return {
+                ...state,
+                auth:false
+            }
+
+        default:
+            return state
+     }
+ }
+
+ const actionCreatorLogin = () =>{
+     return {
+         type: LOGIN
+     }
+ }
+
+const actionCreatorLogout = () => {
+    return {
+        type: LOGOUT
+    }
+}
+
+const store = Redux.createStore(reducer)
